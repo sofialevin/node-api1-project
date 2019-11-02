3ba508ccb60ec0e30abd1cac2346c8a6043a566f
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-11-02 10:09:03 -0700

    Basic server setup.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// implement your API here
+const db = require('./data/db');
+const express = require('express');
+
+const server = express();
+
+server.listen(4000, () => {
+    console.log('=== server listening on port 4000 ===');
+});
+
+server.use(express.json());
+
+server.get('/', (request, response) => {
+    response.send('hello world...');
+});
