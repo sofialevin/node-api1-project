11e7776dc11948eca984c17916cc2d094c7e688e
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-11-02 11:09:10 -0700

    Completed post request, added linter.

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ server.use(express.json());
 server.get('/', (request, response) => {
     response.send('hello world...');
 });
+
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+
+    !newUser.name || !newUser.bio
+    ? res.status(400).json(
+        {
+            success: false,
+            errorMessage: "Please provide name and bio for the user."
+        }
+    )
+    : db.insert(newUser)
+        .then(user => res.status(201).json(
+            {
+                success: true,
+                user
+            }
+        ))
+        .catch(err => res.status(500).json(
+            {
+                success: false,
+                err
+            }
+        ))
+})
