52d9ce4be96f4690a9f2bd6a96808303e6c7ea16
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-02 13:27:06 -0800

    Completed GET on /api/users/:id and updated error messages.

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ server.post('/api/users', (req, res) => {
             })
             .catch(err => {
                 console.log("error on POST /api/users", err);
-                res.status(500).json({ error: "Error creating user."})
+                res.status(500).json({ error: "There was an error while saving the user to the database"})
             })
     } else {
-        res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+        res.status(400).json({error: "Please provide name and bio for the user."})
     }
 })
 
@@ -39,7 +39,24 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => {
         console.log("error on GET /api/users", err);
-        res.status(500).json({ error: "Error getting users."})
+        res.status(500).json({ error: "The users information could not be retrieved."})
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id
+
+    db.findById(id)
+    .then(user => {
+        if (user) {
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({ error: "The user with the specified ID does not exist."})
+        }
+    })
+    .catch(err => {
+        console.log(`error on GET /api/users/${id}`, err);
+        res.status(500).json({ error: "The user information could not be retrieved."})
     })
 })
 
